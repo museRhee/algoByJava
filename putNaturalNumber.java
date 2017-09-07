f4bb6b5406c25f0c08dfb9092d183380a76410fe
museRhee <donggyu.rhee@gmail.com>
2017-09-07 20:11:22 +0900

    puNaturalNumber

--- putNaturalNumber.java
+++ putNaturalNumber.java
@@ -5,12 +5,11 @@ import java.util.*;
 
 public class putNaturalNumber {
 	public static void main(String[] args) throws IOException {
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		String[] line = bf.readLine().split(" ");
-		String a = line[0] + line[1];
-		String b = line[2] + line[3];
+		BufferedReader inputLine = new BufferedReader(new InputStreamReader(System.in));
+		String[] inputNumber = inputLine.readLine().split(" ");
+		String a = inputNumber[0] + inputNumber[1];
+		String b = inputNumber[2] + inputNumber[3];
 		long answer = Long.valueOf(a) + Long.valueOf(b);
-		
-		System.out.println(answer);
-		}
+		System.out.println(answer);	
+	}
 }
